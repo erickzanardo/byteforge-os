483c1f6583bdff0acab68e473fd449bd1c072d49
Erick Zanardo <erickzanardoo@gmail.com>
2016-10-17 15:15:30 -0200

    Improving alarm.js

--- scripts/alarm.js
+++ scripts/alarm.js
@@ -2,18 +2,24 @@
 
 const player = require("play-sound")(opts = {});
 const homepath = require("./homepath");
+const toast = require("./toast");
 
-function alarm(seconds) {
-	if (!seconds) return console.error("No parameter was specified.");
+function alarm(seconds, message) {
+  if (!seconds) return console.error("No parameter was specified.");
 	
-	seconds = parseInt(seconds, 10);
+  seconds = parseInt(seconds, 10);
   if (isNaN(seconds)) return console.error("The seconds paramenter must be a valid integer number.");
 
   seconds = seconds * 1000;
 
   setTimeout(() => {
+    toast.normal("Alarm", message);
     player.play(homepath() + "/.bfos/resources/sfx/alarm.wav", () => console.log("alarm execution successful"));
   }, seconds);
 }
 
-alarm(process.argv[2]);
+if (require.main === module) {
+  alarm(process.argv[2], process.argv[3]);
+} else {
+  module.exports = alarm;
+}

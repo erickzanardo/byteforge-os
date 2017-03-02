c5a9192800a70fad97680e56de911a066e7f8253
Erick <erickzanardoo@gmail.com>
2017-03-02 17:45:31 -0300

    adding silent mode for ski (is not working perfectly yet)

--- system/modules/sky/sky.js
+++ system/modules/sky/sky.js
@@ -1,11 +1,17 @@
 const picoSpeaker = require("pico-speaker");
 const script = require("./scripts/scripts");
+const homepath = require("../homepath");
+const toast = require("../toast");
+
+const fs = require("fs");
+
+const isSilentMode = fs.existsSync(`${homepath()}/.sky-silent-mode`);
 
 const sky = {
   start: () => {
     sky.scriptedPhrase("welcome");
   },
-  say: (text) => picoSpeaker.speak(text),
+  say: (text) => isSilentMode ? Promise.resolve(toast.normal("Sky", text)) : picoSpeaker.speak(text),
   scriptedPhrase: (key) => sky.say(script(key))
 };
 

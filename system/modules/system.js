5b84196104671ae0b2d36f1211bda03a62abaf14
Erick Zanardo <erickzanardoo@gmail.com>
2017-06-13 11:54:59 -0300

    fixing lock action from system module

--- system/modules/system.js
+++ system/modules/system.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   lock: () => {
     sky.scriptedPhrase("lock").then(() => {
-      exec("xrandr | grep \\* | cut -c 4-12 | tail -1", (error, stdout, stderr) => 
+      exec("xrandr | grep \\* | cut -c 4-12 | head -n 1", (error, stdout, stderr) => 
         exec(`convert -resize ${stdout.replace("\n", "")} ~/.bfos/images/lockscreenimage.png /tmp/locksreenimage.png`, () =>
           exec("i3lock -i /tmp/locksreenimage.png -c 000000"))
       );

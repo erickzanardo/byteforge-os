4e9298302ae1e467c4e8910d699fc2944ec50acb
Erick Zanardo <erickzanardoo@gmail.com>
2017-08-23 10:18:01 -0300

    Removing cmus status from threebar and fixing the datetime icons

--- threebar/datetime.js
+++ threebar/datetime.js
@@ -2,9 +2,9 @@ module.exports = function(update) {
   var updateTime = () => {
     var d = new Date();
     update("datetime", [
-        "\uf073",
-        d.toLocaleTimeString("pt-BR"),
         "\uf017",
+        `${d.getHours().toString(2)}:${d.getMinutes().toString(2)}`,
+        "\uf073",
         [
           d.getDate(),
           d.getMonth() + 1,

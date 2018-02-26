a492b2e096ef23c7fe2e6de766cdfb81f2e3d8b2
Erick Zanardo <erickzanardoo@gmail.com>
2018-02-26 11:21:31 -0300

    Fixing system module

--- system/modules/system.js
+++ system/modules/system.js
@@ -4,17 +4,17 @@ const sky = require("./sky/sky");
 module.exports = {
   shutdown: () => {
     sky.scriptedPhrase("shutdown").then(() => {
-      exec("dbus-send --system --print-reply --dest=\"org.freedesktop.ConsoleKit\" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop")
+      exec("shutdown -P 0")
     });
   },
   reboot: () => {
     sky.scriptedPhrase("reboot").then(() => {
-      exec("dbus-send --system --print-reply --dest=\"org.freedesktop.ConsoleKit\" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart");
+      exec("reboot");
     });
   },
   lock: () => {
     sky.scriptedPhrase("lock").then(() => {
-      exec("xrandr | grep \\* | cut -c 4-12 | head -n 1", (error, stdout, stderr) => 
+      exec("xrandr | grep \\* | cut -c 4-12 | head -n 1", (error, stdout, stderr) =>
         exec(`convert -resize ${stdout.replace("\n", "")} ~/.bfos/images/lockscreenimage.png /tmp/locksreenimage.png`, () =>
           exec("i3lock -i /tmp/locksreenimage.png -c 000000"))
       );
